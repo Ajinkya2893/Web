fbf34b19f7cb2dcd9ad4a2882c516a7a33ca0125
Ajinkya2893 <ajinkya@mindsarray.com>
2018-04-09 15:44:07 +0530

    Updated xml and suite details

--- src/test/java/com/base/RemitSuiteRun.java
+++ src/test/java/com/base/RemitSuiteRun.java
@@ -2,27 +2,25 @@ package com.base;
 
 import java.io.File;
 
-import org.testng.annotations.AfterSuite;
-import org.zeroturnaround.zip.ZipUtil;
+import org.testng.annotations.BeforeSuite;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
 import Utility.Constants;
-import Utility.SendMail;
 
 public class RemitSuiteRun {
 
 	public ExtentReports rep;
 	public ExtentTest test;
 
-/*	@BeforeSuite
+	@BeforeSuite
 	public void beforeSuite() {
 		File myFile = new File(Constants.DesPath);
 		if (!myFile.isDirectory()) myFile.delete();
-	}*/
+	}
 	
-	@AfterSuite
+	/*@AfterSuite
 	public void afterSuite() {
 		try {
 			rep.flush();
@@ -31,5 +29,5 @@ public class RemitSuiteRun {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
+	}*/
 }
--- src/test/java/com/base/ShopSuiteRun.java
+++ src/test/java/com/base/ShopSuiteRun.java
@@ -2,34 +2,36 @@ package com.base;
 
 import java.io.File;
 
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+import org.zeroturnaround.zip.ZipUtil;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
 import Utility.Constants;
+import Utility.SendMail;
 
 public class ShopSuiteRun {
 	
 	public ExtentReports rep;
 	public ExtentTest test;
 
-	@BeforeSuite
+/*	@BeforeSuite
 	public void beforeSuite() {
 		File zip = new File(Constants.DesPath);
 		if (!zip.isDirectory()) zip.delete();
 		File report = new File(Constants.SrcPath);
 		if (!report.isDirectory()) report.delete();
-	}
+	}*/
 	
-	/*@AfterSuite
+	@AfterSuite
 	public void afterSuite() {
 		try {
 			rep.flush();
-			//ZipUtil.pack(new File(Constants.SrcPath), new File(Constants.DesPath));
-			//new SendMail().sendMail();
+			ZipUtil.pack(new File(Constants.SrcPath), new File(Constants.DesPath));
+			new SendMail().sendMail();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}*/
+	}
 }
